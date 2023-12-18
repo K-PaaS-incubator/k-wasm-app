53ab2b5b97285e58320fb12fda25e8ca0f45f61d
Hyeopgeon Lee <soft1318@naver.com>
2023-12-18 14:38:48 +0900

    HTML 기반 K-WASM UI 앱

---
+++ ui-html/src/main/java/kr/or/kpaas/kwasm/UiHtmlApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kpaas.kwasm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiHtmlApplication.class, args);
+    }
+
+}
