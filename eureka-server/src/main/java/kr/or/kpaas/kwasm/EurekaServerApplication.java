2f4b8e80f1f09858715bf4102969db9cd863f60a
Hyeopgeon Lee <soft1318@naver.com>
2023-12-18 10:18:58 +0900

    환경 설정 분리를 위해 Config Server 구현

---
+++ eureka-server/src/main/java/kr/or/kpaas/kwasm/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kpaas.kwasm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
