289126213dd844034a9ff883274291cfa2c0f71e
Hyeopgeon Lee <soft1318@naver.com>
2023-12-26 10:27:44 +0900

    관리 서버 라이브러리 수정 및 옵션 추가

--- app-manager/src/main/java/kr/or/kpaas/kwasm/AppManagerApplication.java
+++ app-manager/src/main/java/kr/or/kpaas/kwasm/AppManagerApplication.java
@@ -1,8 +1,10 @@
 package kr.or.kpaas.kwasm;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class AppManagerApplication {
 
