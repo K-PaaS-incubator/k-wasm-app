d7ac2ee2a26eb604db4d19c199bfa4985c9e5c28
Hyeopgeon Lee <soft1318@naver.com>
2023-12-26 11:17:56 +0900

    환경설정 수정

---
+++ api-gateway/src/main/java/kr/or/kpaas/kwasm/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package kr.or.kpaas.kwasm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
