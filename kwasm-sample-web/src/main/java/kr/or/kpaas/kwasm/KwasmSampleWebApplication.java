da453bee4151214e773b4312a88cd9719713d62f
Hyeopgeon Lee <soft1318@naver.com>
2023-12-29 12:52:19 +0900

    K-WASM 결과 샘플로 실행한 샘플 앱

---
+++ kwasm-sample-web/src/main/java/kr/or/kpaas/kwasm/KwasmSampleWebApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kpaas.kwasm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwasmSampleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwasmSampleWebApplication.class, args);
+    }
+
+}
