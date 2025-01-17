0638a69b0d0a50fdeba7939cdf39ee8e6c0d9673
Hyeopgeon Lee <soft1318@naver.com>
2025-01-17 17:38:46 +0900

    RAM 서비스 추가

---
+++ rag-llm/src/main/java/kopo/poly/RagLlmApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagLlmApplication.class, args);
+    }
+
+}
